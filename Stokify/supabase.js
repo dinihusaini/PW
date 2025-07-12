d828086ed6c13e1cde5a82817aa86818bc34597b
Dini Husaini <dinihusaini2312@gmail.com>
2025-07-12 22:13:26 +0700

    Update supabase.js

--- Stokify/supabase.js
+++ Stokify/supabase.js
@@ -1,3 +1,6 @@
-const SUPABASE_URL = 'https://xxxx.supabase.co';
-const SUPABASE_KEY = 'public-anon-key';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const SUPABASE_URL = 'https://tfvzsriecpzvrnzlvwww.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
